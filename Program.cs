86d7a2658482a1c753452c387e8b5597e7828933
gamelecture2024 <dlfjswodlf@naver.com>
2024-02-08 16:06:41 +0900

    (2024.02.08) First Commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2024CSStudyKDH
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
